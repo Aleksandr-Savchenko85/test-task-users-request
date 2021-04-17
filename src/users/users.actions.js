0f7b4444b052c52e3e5727f0e238ebced161ba25
tests2 <https://github.com/roman1501/tests2.git>
2021-04-17 16:46:18 +0300

    commit

--- src/users/users.actions.js
+++ src/users/users.actions.js
@@ -4,6 +4,9 @@ import { getUsersList } from './components/gateway';
 export const GO_PREV = "USER/PREV";
 export const GO_NEXT = "USER/NEXT";
 export const GET_USERS = "GET_USERS";
+export const CREATE_USERS = "GET_USERS";
+export const UPDATE_USERS = "GET_USERS";
+export const DELETE_USERS = "GET_USERS";
 
 export const goPrev = () => {
   return {
