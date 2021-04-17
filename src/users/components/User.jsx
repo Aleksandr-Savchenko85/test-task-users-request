c9ff6ce9cf4e42702aa629c25c26b000d252f05b
tests2 <https://github.com/roman1501/tests2.git>
2021-04-18 00:02:18 +0300

    commit

--- src/users/components/User.jsx
+++ src/users/components/User.jsx
@@ -5,8 +5,8 @@ const User = (props) => {
     return (
 
         <li className="user">
-            <button className="delete-btn edit-btn" onClick={() =>console.log('Edit form')}>edit</button>
-            <button className="delete-btn" onClick={()=> console.log("Delete user")}>delete</button>
+            <button className="delete-btn edit-btn" onClick={() => console.log('Edit user')}>edit</button>
+            <button className="delete-btn" onClick={() => console.log("Delete user")}>delete</button>
             <span className="user__id sp"> <span className="color-letter">ID: </span> {props.id}</span >
             <span className="user__name sp"><span className="color-letter">Name: </span>{props.name}</span>
             <span className="user__surname sp"><span className="color-letter">Surname: </span>{props.surname}</span>
